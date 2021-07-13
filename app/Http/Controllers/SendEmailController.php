b358789dc13925094ce439a55040206b8a9fb07b
Fahrican Kaçan <fahrican.kcn@gmail.com>
2021-07-14 01:28:22 +0300

    custom email desteği hatası düzeltildi

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -30,7 +30,7 @@ class SendEmailController extends Controller
             ]
             );
 
-            for($i=count($request->musteri_id);$i>0;$i--){
+            for($i=count($request->musteri_id);$i>=0;$i--){
 
                 if(!empty($request->musteri_id[$i-1])){
                     $musteri=Personel::find($request->musteri_id[$i-1]);
@@ -57,7 +57,7 @@ class SendEmailController extends Controller
             ]
             );
 
-        for($i=count($request->musteri_id);$i>0;$i--){
+        for($i=count($request->musteri_id);$i>=0;$i--){
 
             if(!empty($request->musteri_id[$i-1])){
                 $musteri=Musteri::find($request->musteri_id[$i-1]);
@@ -75,6 +75,20 @@ class SendEmailController extends Controller
 
 
     public function SendMailToCustomPerson(Request $request){
+
+        $validate = $request->validate([
+            'mail_adresi'=> 'required',
+            'mail_aciklamasi'=>'required',
+            'mail_adi'=> 'required',
+        ]);
+
+        $mailAdresleri = explode(",",$request->mail_adresi[0]);
+
+        if(!empty($mailAdresleri)){
+            for($i=count($mailAdresleri)-1;$i>=0;$i--){
+                Mail::to($mailAdresleri[$i])->send(new Contact($request->mail_aciklamasi));
+            }
+        }
 
         return redirect('/iletisim/index');
     }
--- resources/views/iletisim/iletisim_custom.blade.php
+++ resources/views/iletisim/iletisim_custom.blade.php
@@ -2,60 +2,58 @@
 
 @section('content')
 
-    <form action="/sendemail/custom" method="post">
-        @csrf
-        <div class="card">
-            <div class="card-body">
+<form action="/sendemail/custom" method="post">
+    @csrf
+    <div class="card">
+        <div class="card-body">
 
-                <fieldset class="mb-3">
-                    <legend class="text-uppercase font-size-sm font-weight-bold">Mail Oluştur</legend>
+            <fieldset class="mb-3">
+                <legend class="text-uppercase font-size-sm font-weight-bold">Mail Oluştur</legend>
 
-                    <div class="form-group row">
-                        <label class="col-form-label col-lg-2" for="custom_mail_id">Mail Adresi Gir</label>
-                        <div class="col-lg-10">
-                            <input type="email" id="custom_mail_id" placeholder="Email" name="mail_adi[]" multiple="multiple">
+                <div class="form-group row">
+                    <label class="col-form-label col-lg-2">Mail Başlığı</label>
+                    <div class="col-lg-10">
+                        <input type="email" class="form-control" name="mail_adresi[]" multiple="multiple">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-form-label col-lg-2">Mail Başlığı</label>
+                    <div class="col-lg-10">
+                        <input type="text" class="form-control" name="mail_adi">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-form-label col-lg-2">Mail Açıklaması</label>
+                    <div class="col-lg-10">
+                        <textarea rows="3" cols="3" class="form-control" placeholder="MAİL Açıklaması"
+                            name="mail_aciklamasi"></textarea>
+                    </div>
+                </div>
 
+
+                @if ($errors->any())
+                    <div class="alert alert-danger" role="alert">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                <div>
+                    <div class="row justify-content-end">
+                    
+                        <div class=" p-1">
+                            <button type="submit" class="btn btn-primary">Mail Gönder <i
+                                    class="icon-paperplane ml-2"></i></button>
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label class="col-form-label col-lg-2">Mail Başlığı</label>
-                        <div class="col-lg-10">
-                            <input type="text" class="form-control" name="mail_adi">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-form-label col-lg-2">Mail Açıklaması</label>
-                        <div class="col-lg-10">
-                            <textarea rows="3" cols="3" class="form-control" placeholder="MAİL Açıklaması"
-                                name="mail_aciklamasi"></textarea>
-                        </div>
-                    </div>
-
-
-                    @if ($errors->any())
-                        <div class="alert alert-danger" role="alert">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-
-                    <div>
-                        <div class="row justify-content-end">
-
-                            <div class=" p-1">
-                                <button type="submit" class="btn btn-primary">Mail Gönder <i
-                                        class="icon-paperplane ml-2"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </fieldset>
-            </div>
+                </div>
+            </fieldset>
         </div>
-    </form>
-
+    </div>
+</form>
 
 
 @endsection
